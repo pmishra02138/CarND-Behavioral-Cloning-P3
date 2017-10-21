d0d4425acd102d25c105b3d99b1a50222f9f1a8d
Pankaj Mishra <pmishra02138@gmail.com>
2017-10-21 15:08:01 +0530

    Crop top and bottom of images

--- createModel.py
+++ createModel.py
@@ -13,14 +13,15 @@ images = []
 measurements = []
 
 for line in lines:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    current_path = 'sim_data/IMG/' + filename
-    image = ndimage.imread(current_path)
-    # image = cv2.imread(current_path)
-    images.append(image)
-    measurement = float(line[3])
-    measurements.append(measurement)
+    for i in range(3):
+        source_path = line[i]
+        filename = source_path.split('/')[-1]
+        current_path = 'sim_data/IMG/' + filename
+        image = ndimage.imread(current_path)
+        # image = cv2.imread(current_path)
+        images.append(image)
+        measurement = float(line[3])
+        measurements.append(measurement)
 
 # Augmented images
 augmented_images, augmented_measurements = [], []
@@ -35,10 +36,11 @@ y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
-from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Convolution2D, MaxPooling2D, Cropping2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
 model.add(Convolution2D(6,5,5,activation="relu"))
 model.add(MaxPooling2D())
 model.add(Convolution2D(6,5,5,activation="relu"))
