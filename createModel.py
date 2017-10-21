b0cc34d2230dbeee3c77ebb016fb36faee00fa61
Pankaj Mishra <pmishra02138@gmail.com>
2017-10-21 14:30:47 +0530

    Add augemented images

---
+++ createModel.py
@@ -0,0 +1,53 @@
+import csv
+from scipy import ndimage
+# import cv2
+import numpy as np
+lines = []
+
+with open('sim_data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+
+for line in lines:
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = 'sim_data/IMG/' + filename
+    image = ndimage.imread(current_path)
+    # image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+# Augmented images
+augmented_images, augmented_measurements = [], []
+for image,measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(np.fliplr(image))
+    augmented_measurements.append(-measurement)
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Convolution2D, MaxPooling2D
+
+model = Sequential()
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
+model.save('model.h5')
