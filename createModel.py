b07d2900fbca83dde3c8e2d5767c0869a4fecff6
Pankaj Mishra <pmishra02138@gmail.com>
2017-10-24 08:31:04 +0530

    Add generator function (No augmented data)

--- createModel.py
+++ createModel.py
@@ -1,38 +1,60 @@
 import csv
 from scipy import ndimage
-# import cv2
+import cv2
 import numpy as np
-lines = []
+import sklearn
 
+samples = []
 with open('sim_data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
-        lines.append(line)
+        samples.append(line)
 
-images = []
-measurements = []
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
-for line in lines:
-    for i in range(3):
-        source_path = line[i]
-        filename = source_path.split('/')[-1]
-        current_path = 'sim_data/IMG/' + filename
-        image = ndimage.imread(current_path)
-        # image = cv2.imread(current_path)
-        images.append(image)
-        measurement = float(line[3])
-        measurements.append(measurement)
+def augment_data(images, measurements):
+    # Augmented inputs
+    augmented_images, augmented_measurements = [], []
+    for image,measurement in zip(images, measurements):
+        augmented_images.append(image)
+        augmented_measurements.append(measurement)
+        augmented_images.append(np.fliplr(image))
+        augmented_measurements.append(-measurement)
 
-# Augmented images
-augmented_images, augmented_measurements = [], []
-for image,measurement in zip(images, measurements):
-    augmented_images.append(image)
-    augmented_measurements.append(measurement)
-    augmented_images.append(np.fliplr(image))
-    augmented_measurements.append(-measurement)
+    output = {'augmented_images': augmented_images, \
+                'augemented_angles': augmented_measurements}
+    return output
 
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        sklearn.utils.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                # for i in range(3):
+                name = 'sim_data/IMG/'+batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+            # augment images
+            # output = augment_data(images, angles)
+            # X_train = np.array(output['augmented_images'])
+            # y_train = np.array(output['augemented_angles'])
+            X_train = np.array(images)
+            y_train = np.array(angles)
+
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
@@ -53,5 +75,7 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
+model.fit_generator(train_generator, samples_per_epoch= \
+            len(train_samples), validation_data=validation_generator, \
+            nb_val_samples=len(validation_samples), nb_epoch=10)
 model.save('model.h5')
