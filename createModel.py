cf957cba41f51a8130ff161e1f335cd4a6f96953
Pankaj Mishra <pmishra02138@gmail.com>
2017-10-25 21:44:44 +0530

    Add majo comments

--- createModel.py
+++ createModel.py
@@ -4,12 +4,14 @@ import cv2
 import numpy as np
 import sklearn
 
+# Read in sample data names from log file
 samples = []
 with open('sim_data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
 
+# Read images and add correction factor
 images = []
 measurements = []
 correction = [0.0, 0.2, -0.2]
@@ -39,6 +41,7 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
 from keras.layers import Convolution2D, MaxPooling2D, Cropping2D
 
+# NVIDIA model
 model = Sequential()
 model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
@@ -53,6 +56,7 @@ model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 
+# Train and save model
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
 model.save('model.h5')
